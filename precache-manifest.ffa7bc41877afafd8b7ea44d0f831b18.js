976f76540244c9a72f177493cbeefc5af4cd5649
arihant <ajarihant>
2020-12-05 04:28:17 +0530

    Updates

---
+++ precache-manifest.ffa7bc41877afafd8b7ea44d0f831b18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "58e205ca92af7ea7c854",
+    "url": "/portfolio/static/js/main.58e205ca.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "58e205ca92af7ea7c854",
+    "url": "/portfolio/static/css/main.1b21ce94.chunk.css"
+  },
+  {
+    "revision": "7f34f6127fccd9f193a3c32b0553b415",
+    "url": "/portfolio/index.html"
+  }
+];
